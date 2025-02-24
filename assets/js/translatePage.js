b7d620720ddb3885a425e50e2fa6299cd185251a
AntSur1 <ant.surminski@gmail.com>
2025-02-25 00:26:29 +0100

    fixed broken translation

--- assets/js/translatePage.js
+++ assets/js/translatePage.js
@@ -2,10 +2,14 @@ function toggleLanguage() {
   const currentUrl = window.location.href;
   let newUrl;
 
-  if (currentUrl.includes("/en/")) {
+  if (currentUrl == "https://nataliadziuk.pl/") {
+    newUrl = "https://nataliadziuk.pl/en/index";
+  }
+  else if (currentUrl.includes("/en/")) {
     // If the URL contains "/en/", remove it and redirect to Polish version
     newUrl = currentUrl.replace("/en/", "/");
-  } else {
+  } 
+  else {
     // If the URL doesn't contain "/en/", add "/en/" to the path and redirect to English version
     if (currentUrl.includes("/")) {
       newUrl = currentUrl.replace(window.location.pathname, "/en" + window.location.pathname);
