dcc5bbeb3ad663ca1a66d925acfe59734b444443
AntSur1 <ant.surminski@gmail.com>
2025-03-26 23:00:33 +0100

    cleaned up debug mess, language button works though :+1:

--- assets/js/translatePage.js
+++ assets/js/translatePage.js
@@ -1,14 +1,12 @@
 function toggleLanguage() {
   const currentUrl = window.location.href;
-  const enUrl = currentUrl.replace(currentUrl, currentUrl +"en/index");
   let newUrl;
 
   console.log(6);
   console.log(window.location.pathname);
-  console.log(enUrl);
   if (window.location.pathname == "/") {
     console.log("debug");
-    newUrl = enUrl;
+    newUrl = currentUrl.replace(currentUrl, currentUrl +"en/index");
   }
   else if (currentUrl.includes("/en/")) {
     // If the URL contains "/en/", remove it and redirect to Polish version
