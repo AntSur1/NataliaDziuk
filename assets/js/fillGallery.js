fbb3bc791d626fa0eea926e3f9bd868267d10436
AntSur1 <ant.surminski@gmail.com>
2025-04-02 21:41:34 +0200

    removed unnecessary fillGallery code

--- assets/js/fillGallery.js
+++ assets/js/fillGallery.js
@@ -1,7 +1,34 @@
 // https://github.com/nextapps-de/spotlight?tab=readme-ov-file
 
 const selectedImagePath = "/assets/images/gallery/featured/";
-const selectedImageData = [
+const selectedImageDataEN = [
+  {path: selectedImagePath + "Rose Garden.jpg", title: "Rose Garden", desc:"A painting on cardboard using traditional acrylic paints, depicting a botanical garden bathed in sunlight."},
+  {path: selectedImagePath + "Basement.jpg", title: "Basement", desc:"A painting created with acrylic paints on paper, depicting the basement of an old, abandoned house."},
+  {path: selectedImagePath + "Bathroom.jpg", title: "Bathroom", desc:"A drawing on paper created with dry pastels."},
+  {path: selectedImagePath + "Concentration.jpg", title: "Concentration", desc:"A portrait of a girl drawn on paper with pencils."},
+  {path: selectedImagePath + "Colorful Hummingbird.jpg", title: "Colorful Hummingbird", desc:"The second version of the hummingbird. A painting on paper created with acrylic paints."},
+  {path: selectedImagePath + "Grandma.jpg", title: "Grandma", desc:"A work created with oil pastels for the 1st National Competition 'Pastel Portrait,' named after Krzysztof Krzych. It won the Grand Prix award in 2024."},
+  {path: selectedImagePath + "Illusion.jpg", title: "Illusion", desc:"A self-portrait created with oil pastels for the 2nd National Competition 'Pastel Portrait,' named after Krzysztof Krzych."},
+  {path: selectedImagePath + "Dolly.jpg", title: "Dolly", desc:"A digital portrait depicting the beauty of aged objects."},
+  {path: selectedImagePath + "Passion.jpg", title: "Passion", desc:"A self-portrait drawn with oil pastels."},
+  {path: selectedImagePath + "Love of a Mother.jpg", title: "Love of a Mother", desc:"A commissioned portrait of a mother and daughter, drawn with pencils."},
+  {path: selectedImagePath + "Affection.jpg", title: "Affection", desc:"A commissioned painting for a couple more than 30 years after their wedding. I used acrylic paints on canvas."},
+  {path: selectedImagePath + "Never Enough.jpg", title: "Never Enough", desc:"A digital portrait depicting a young woman looking at everything and everyone with superiority."},
+  {path: selectedImagePath + "Light Spirit.jpg", title: "Light Spirit", desc:"A geometric digital illustration of a cheerful little soul, created as a profile picture."},
+  {path: selectedImagePath + "Hope.jpg", title: "Hope", desc:"A work created for the review in Koszalin in 2023. The portrait was drawn with pencils."},
+  {path: selectedImagePath + "Hope of Tomorrow.jpg", title: "Hope of Tomorrow", desc:"An outdoor painting created with oil paints on canvas. It was part of a post-plein-air exhibition."},
+  {path: selectedImagePath + "Infinite Knowledge.jpg", title: "Infinite Knowledge", desc:"A digital illustration depicting a figure with the essence of a heavenly creator."},
+  {path: selectedImagePath + "Still Life.jpg", title: "Still Life", desc:"A work created with acrylics on canvas."},
+  {path: selectedImagePath + "Timid Timmy.jpg", title: "Timid Timmy", desc:"A digital portrait of a young boy seeking affection from his parents."},
+  {path: selectedImagePath + "Pose 1.jpg", title: "Pose 1", desc:"A drawing of a young woman posing. Created with pencils and graphite."},
+  {path: selectedImagePath + "Pose 2.jpg", title: "Pose 2", desc:"A drawing of a young woman posing. Created with pencils and graphite."},
+  {path: selectedImagePath + "Ruined King.jpg", title: "Ruined King", desc:"An original portrait of Viego from League of Legends, drawn with a black pen."},
+  {path: selectedImagePath + "Hummingbird.jpg", title: "Hummingbird", desc:"A drawing of a bird, created with a pencil."},
+  {path: selectedImagePath + "Neverland.jpg", title: "Neverland", desc:"A panorama of my fantasy interpretation of Neverland. I used ink, pen drawing, and the wash technique for this piece."},
+  {path: selectedImagePath + "Mural.jpg", title: "Mural", desc:"A commissioned project created in the town of Niewodniki (Poland). It references the town’s history and was created digitally."},
+];
+
+const selectedImageDataPL = [
   {path: selectedImagePath + "Rose Garden.jpg", title: "Ogród Różany", desc:"Obraz na kartonie wykonany tradycyjnymi farbami akrylowymi, przedstawiający ogród botaniczny skąpany w świetle słonecznym."},
   {path: selectedImagePath + "Basement.jpg", title: "Piwnica", desc:"Obraz wykonany farbami akrylowymi na papierze, przedstawiający piwnicę starego, opuszczonego domu."},
   {path: selectedImagePath + "Bathroom.jpg", title: "Łazienka", desc:"Rysunek na papierze wykonany suchymi pastelami."},
@@ -28,8 +55,6 @@ const selectedImageData = [
   {path: selectedImagePath + "Mural.jpg", title: "Mural", desc:"Zlecony projekt stworzony do miejscowości Niewodniki. Odnosi się do historii miejscowości i został stworzony cyfrowo."},
 ];
 
-
-
 const sketchImagePath = "/assets/images/gallery/sketches/";
 const sketchImageData = [
   {path: sketchImagePath + "28.jpg", desc:"Test desc"},
@@ -43,6 +68,13 @@ const sketchImageData = [
   {path: sketchImagePath + "26.jpg", desc:"Test desc"},
   {path: sketchImagePath + "27.jpg", desc:"Test desc"},
 ];
+
+const href = window.location.href;
+let selectedImageData = selectedImageDataPL;
+
+if (href.includes("/en/")) {
+  selectedImageData = selectedImageDataEN;
+}
 
 
 function reLoadGallery(imageData, shouldDisplayInfo) {
