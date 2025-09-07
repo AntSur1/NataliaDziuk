5b33b32a3989e540ffc3776d36e8a8351a71b98d
AntSur1 <ant.surminski@gmail.com>
2025-09-07 14:37:41 +0200

    Updates

---
+++ assets/js/translatePage.js
@@ -0,0 +1,25 @@
+function toggleLanguage() {
+  const currentUrl = window.location.href;
+  let newUrl;
+
+  if (window.location.pathname == "/") {
+    console.log("debug");
+    newUrl = currentUrl.replace(currentUrl, currentUrl +"en/index");
+  }
+  else if (currentUrl.includes("/en/")) {
+    // If the URL contains "/en/", remove it and redirect to Polish version
+    newUrl = currentUrl.replace("/en/", "/");
+  } 
+  else {
+    // If the URL doesn't contain "/en/", add "/en/" to the path and redirect to English version
+    if (currentUrl.includes("/")) {
+      newUrl = currentUrl.replace(window.location.pathname, "/en" + window.location.pathname);
+    } 
+    else {
+      newUrl = currentUrl + "/en";
+    }
+  }
+
+  // Redirect to the new URL
+  window.location.href = newUrl;
+}
