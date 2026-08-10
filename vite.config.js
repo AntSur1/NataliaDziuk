ba52915be88c2900f2596a9d2c9f4cabfa3a2e0b
AntSur1 <113517280+AntSur1@users.noreply.github.com>
2026-08-10 23:10:12 +0200

    fixed vite js file compilation dependency

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,16 @@
 // vite.config.js
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 
 export default defineConfig({
   base: '/', // Must match your repo name exactly
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        gallery: resolve(__dirname, 'gallery.html'),
+        enGallery: resolve(__dirname, 'en/gallery.html'),
+      },
+    },
+  },
 })
